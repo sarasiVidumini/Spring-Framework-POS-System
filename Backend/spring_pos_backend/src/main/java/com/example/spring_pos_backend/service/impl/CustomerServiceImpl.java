b5bfc0a1f77fcb66117e9fe2588e3907505b1aaf
thilina-fer <dilshanthilina1010@gmail.com>
2026-02-07 12:09:29 +0530

    feat(backend) : create a saveCustomer method in CustomerServiceImpl class

--- Backend/spring_pos_backend/src/main/java/com/example/spring_pos_backend/service/impl/CustomerServiceImpl.java
+++ Backend/spring_pos_backend/src/main/java/com/example/spring_pos_backend/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,37 @@
 package com.example.spring_pos_backend.service.impl;
 
-public class CustomerServiceImpl {
+import com.example.spring_pos_backend.dto.CustomerDTO;
+import com.example.spring_pos_backend.entity.Customer;
+import com.example.spring_pos_backend.repository.CustomerRepo;
+import com.example.spring_pos_backend.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+    @Service
+    @RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+        private final CustomerRepo customerRepo;
+    @Override
+    public void saveCustomer(CustomerDTO customerDTO) {
+        customerRepo.save(new Customer(
+                customerDTO.getCId(),
+                customerDTO.getCName(),
+                customerDTO.getCAddress()
+        ));
+    }
+
+    @Override
+    public void updateCustomer(CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String customerId) {
+
+    }
+
+    @Override
+    public void getAllCustomer(CustomerDTO customerDTO) {
+
+    }
 }
