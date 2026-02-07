d730e11c015ac566bde615f2d213e7c8158cb3f6
thilina-fer <dilshanthilina1010@gmail.com>
2026-02-07 10:35:51 +0530

    feat(backend) : create a CustomerEntity class for manage database

--- Backend/spring_pos_backend/src/main/java/com/example/spring_pos_backend/entity/CustomerEntity.java
+++ Backend/spring_pos_backend/src/main/java/com/example/spring_pos_backend/entity/CustomerEntity.java
@@ -1,7 +1,19 @@
-//package com.example.spring_pos_backend.entity;
-//
-//public class CustomerEntity {
-//    private String cId;
-//    private String cName;
-//    private String cAddress;
-//}
+package com.example.spring_pos_backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class CustomerEntity {
+    @Id
+    private String cId;
+    private String cName;
+    private String cAddress;
+}
