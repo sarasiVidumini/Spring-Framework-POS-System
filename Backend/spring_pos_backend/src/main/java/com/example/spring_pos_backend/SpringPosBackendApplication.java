4d42ce31a4c73a1fea027d479f076509d9069662
thilina-fer <dilshanthilina1010@gmail.com>
2026-02-07 09:06:55 +0530

    feat(backend) : create a springboot project and add a mysql , spring web mvx..etc dependencies

---
+++ Backend/spring_pos_backend/src/main/java/com/example/spring_pos_backend/SpringPosBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring_pos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPosBackendApplication.class, args);
+    }
+}
