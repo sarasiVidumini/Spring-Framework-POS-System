9fbcfdf42b9a69c8855738604a9a162b8847827a
thilina-fer <dilshanthilina1010@gmail.com>
2026-02-11 08:55:52 +0530

    feat(backend) : create a item  entity class

---
+++ Backend/spring_pos_backend/src/main/java/com/example/spring_pos_backend/entity/item.java
@@ -0,0 +1,17 @@
+package com.example.spring_pos_backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class item {
+    @Id
+    private String code;
+    private String description;
+    private double unitPrice;
+    private int qtyonHand;
+}
